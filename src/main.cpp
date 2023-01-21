6ea6a00732dcb124a207904cc9f6e3e7749dbc36
Taufan Augusta <taufan.augusta@protonmail.com>
2023-01-21 12:23:58 +0700

    tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
 #include <Arduino.h>
+#include <DNSServer.h>
 
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
+	Serial.begin(115200);
+	pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+	// digitalWrite(LED_BUILTIN, LOW);
+	// Serial.println("LOW");
+	// delay(100);
+
+	// digitalWrite(LED_BUILTIN, HIGH);
+	// Serial.println("HIGH");
+	// delay(100);
 }
