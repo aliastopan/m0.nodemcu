e81264a078c71833e6a85ba621c00ff6a9dcf2be
Taufan Augusta <taufan.augusta@protonmail.com>
2023-01-25 10:34:41 +0700

    rest.api

--- include/rest.api.h
+++ include/rest.api.h
@@ -1,7 +1,14 @@
 #ifndef RESTAPI
 #define RESTAPI
 
+#include <ArduinoJson.h>
 #include <server.config.h>
+
+DynamicJsonDocument doc(256);
+char json_string[256];
+
+int mq3_analog = 0;
+int mq3_digital = 0;
 
 class API
 {
@@ -24,8 +31,20 @@ class API
             request->send(200, "application/json", "ready");
         });
 
+        server.on("/mq3", HTTP_GET, [](AsyncWebServerRequest *request){
+            request->send(200, "application/json", json_string);
+        });
+
         server.onNotFound(NotFound);
         server.begin();
+    }
+
+    static void Loop()
+    {
+        doc["analog"] = 0;
+        doc["digital"] = 0;
+
+        serializeJson(doc, json_string);
     }
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,66 +1,34 @@
 #include <Arduino.h>
 #include <rest.api.h>
 
-const int MQ3 = A0;
+const int MQ3_ANALOG = A0;
+const int MQ3_DIGITAL = D1;
+const int WARNING_PIN = D5;
+
 int value = 0;
 
 void setup()
 {
 	Serial.begin(115200);
 	pinMode(LED_BUILTIN, OUTPUT);
-	pinMode(MQ3, INPUT);
-	pinMode(D5, INPUT);
+	pinMode(MQ3_ANALOG, INPUT);
+	pinMode(MQ3_DIGITAL, INPUT);
+	pinMode(WARNING_PIN, OUTPUT);
 
-    // API::Setup();
-    // API::Start();
+    API::Setup();
+    API::Start();
 }
-
-unsigned long start = 0;
-unsigned long timer = 0;
-unsigned long duration = 1000 * 10;
-bool checking = false;
 
 void loop()
 {
+	mq3_analog = analogRead(MQ3_ANALOG);
+	mq3_digital = digitalRead(MQ3_DIGITAL);
+
+	if(mq3_digital == 1)
+		digitalWrite(WARNING_PIN, HIGH);
+	else
+		digitalWrite(WARNING_PIN, LOW);
+
+	API::Loop();
 	delay(1000);
-
-	if(!checking)
-	{
-		value = analogRead(MQ3);
-		Serial.print(value);
-		Serial.print("\t");
-		Serial.println(digitalRead(D5));
-	}
-
-	if(digitalRead(D5) == 0 && !checking)
-	{
-		start = millis();
-		checking = true;
-	}
-
-	if(checking)
-	{
-		int second = start + duration - timer;
-		Serial.print("Checking: ");
-		Serial.print(second/1000);
-		Serial.println(" second(s)");
-
-		if(digitalRead(D5) == 1)
-
-		timer = millis();
-		if(timer >= start + duration)
-		{
-			checking = false;
-		}
-	}
-
-		// unsigned long = start + duration - timer;
-
-	// digitalWrite(LED_BUILTIN, LOW);
-	// Serial.println("LOW");
-	// delay(100);
-
-	// digitalWrite(LED_BUILTIN, HIGH);
-	// Serial.println("HIGH");
-	// delay(100);
 }
